bd7fa8add6758adf73a112375365f92dad7ea876
oscareng <orolf001@ucr.edu>
2023-01-10 01:27:04 -0800

    Initial Commit

---
+++ Experience/Utils/assets.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    name: "room",
+    type: "glbModel",
+    path: "/models/matcharoom.glb",
+  },
+  {
+    name: "screen",
+    type: "videoTexture",
+    path: "/textures/kda.mp4",
+  },
+];
